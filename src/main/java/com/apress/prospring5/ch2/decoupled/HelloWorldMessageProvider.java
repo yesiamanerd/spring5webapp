0747ac988d582cd66b279139c6ec2f1b9e0b194e
yesiamanerd <hepsubah>
2020-04-13 14:27:42 -0400

    commit

--- src/main/java/com/apress/prospring5/ch2/decoupled/HelloWorldMessageProvider.java
+++
@@ -1,8 +0,0 @@
-package com.apress.prospring5.ch2.decoupled;
-
-public class HelloWorldMessageProvider implements MessageProvider{
-    @Override
-    public String getMessage(){
-        return "Hello World!";
-    }
-}
